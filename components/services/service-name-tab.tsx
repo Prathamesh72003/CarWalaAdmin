6f7f148999887e07edbb9b27621bf9bf46c2bf6f
ajinkya-tambe <ajinkyatambe32@gmail.com>
2025-02-28 16:33:17 +0530

    Added beeping dots in Ongoing and Completed Services

--- components/services/service-name-tab.tsx
+++ components/services/service-name-tab.tsx
@@ -1,22 +1,46 @@
-"use client"
-import type { ServiceOption } from "@/components/dashboard/dashboard"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { ServiceTable } from "@/components/services/service-table"
-import { mockOngoingServices, mockCompletedServices } from "@/data/mock-services"
+"use client";
+import type { ServiceOption } from "@/components/dashboard/dashboard";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { ServiceTable } from "@/components/services/service-table";
+import {
+  mockOngoingServices,
+  mockCompletedServices,
+} from "@/data/mock-services";
 
 interface ServiceNameTabProps {
-  service: ServiceOption
+  service: ServiceOption;
 }
 
-export function ServiceNameTab({ service }: ServiceNameTabProps) {
+export function ServiceNameTab({}: ServiceNameTabProps) {
   return (
     <Tabs defaultValue="ongoing" className="w-full">
       <TabsList className="mb-4">
-        <TabsTrigger value="ongoing" data-tab-name="ongoing" className="px-4 cursor-pointer">
+        <TabsTrigger
+          value="ongoing"
+          data-tab-name="ongoing"
+          className="px-4 cursor-pointer"
+        >
           Ongoing Services
+          <div className="pl-5">
+            <span className="flex h-3 w-3 ">
+              <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-green-400 opacity-75"></span>
+              <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
+            </span>
+          </div>
         </TabsTrigger>
-        <TabsTrigger value="completed" data-tab-name="completed" className="px-4 cursor-pointer">
+
+        <TabsTrigger
+          value="completed"
+          data-tab-name="completed"
+          className="px-4 cursor-pointer"
+        >
           Completed Services
+          <div className="pl-5">
+            <span className="flex h-3 w-3">
+              <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-yellow-400 opacity-75"></span>
+              <span className="relative inline-flex rounded-full h-3 w-3 bg-yellow-500"></span>
+            </span>
+          </div>
         </TabsTrigger>
       </TabsList>
 
@@ -28,6 +52,5 @@ export function ServiceNameTab({ service }: ServiceNameTabProps) {
         <ServiceTable data={mockCompletedServices} isOngoing={false} />
       </TabsContent>
     </Tabs>
-  )
+  );
 }
-
